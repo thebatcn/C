660940dea383aa19b6a17ecd52d95889f9a92f04
thebatcn <thebatcn1@hotmail.com>
2023-11-23 20:42:37 +0800

    1

--- c1.c
+++ c1.c
@@ -1,11 +1,10 @@
-typedef int Array[10];
+#include<stdio.h>
 
-Array a;
-
-int main() {
-    int i;
-    for (i = 0; i < 10; i++) {
-        a[i] = i;
-    }
-    return 0;   
+int main(int argc, char const *argv[])
+{
+	int a = 1;
+    printf("请输入一个十进制数：");
+	scanf("%d",&a);
+	printf("十进制%d转化为十六进制%x",a,a);
+	return 0;
 }
